9dd4d657937f021ad96fcec2bbc8e44fd6b8acb2
RonnieTG <67820322+RonnieTG@users.noreply.github.com>
2020-07-07 23:21:16 +0300

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-#add fix to exercise3 here
+sudo su apt-get install sshpass
+sleep 15
+echo "192.168.100.11 server2" >> /etc/hosts
+sshpass -p "vagrant" ssh -o StrictHostKeyChecking=no vagrant@server2
